31b15c07545ac01fc2e80a05dbfe49e5d1a7dab2
ThadDuval <ThadDuval.lavuD@gmail.com>
2015-06-07 17:45:09 -0400

    All Files

---
+++ script.sh
@@ -0,0 +1,6 @@
+echo "# singlePage" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/ThadDuval/singlePage.git
+git push -u origin master
